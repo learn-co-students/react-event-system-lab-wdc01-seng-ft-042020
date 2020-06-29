e09140880ab289c349c900c0f7bd235c0d86ba8f
epittab <ep7m@me.com>
2020-06-29 12:19:30 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+
+import React from 'react'
+
+class EyesOnMe extends React.Component{
+
+    good = () => {
+        console.log('Good!')
+    }
+
+    bad = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render(){
+        return(
+            <div className='EyesOnMe'>
+
+                <button onFocus={this.good} onBlur={this.bad}>Button</button>
+
+            </div>
+        )
+    }
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+
+import React from 'react'
+
+class Keypad extends React.Component{
+
+    pass = () => {
+        console.log('Entering password...')
+    }
+
+    render(){
+        return(
+            <div className='Keypad'>
+                <input type='password' onKeyUp={this.pass}/>
+
+            </div>
+        )
+    }
+
+}
+
+export default Keypad;
